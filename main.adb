bceaa3bcfbe7b3030ab4706ecd504e9dbfdb34f9
CamCatTay <182511057+CamCatTay@users.noreply.github.com>
2026-04-09 14:23:37 -0400

    replace random input with user input

--- main.adb
+++ main.adb
@@ -1,18 +1,37 @@
-with Ada.Numerics.Discrete_Random;
 with Complex_Numbers;
+with Ada.Wide_Text_IO;
+with Ada.Text_IO;
+use Ada.Text_IO;
+with Ada.Integer_Text_IO;
+use Ada.Integer_Text_IO;
 
 procedure Main is
-   subtype Int_Range is Integer range -10 .. 10;
-   package Rand is new Ada.Numerics.Discrete_Random (Int_Range);
-
-   Gen : Rand.Generator;
+   A   : Integer;
+   B   : Integer;
+   C   : Integer;
+   D   : Integer;
    C1  : Complex_Numbers.Complex;
    C2  : Complex_Numbers.Complex;
 begin
-   Rand.Reset (Gen);
 
-   C1 := (Real => Rand.Random (Gen), Imag => Rand.Random (Gen));
-   C2 := (Real => Rand.Random (Gen), Imag => Rand.Random (Gen));
+   Put("C1 Real Part: ");
+   Get(A);
+   Skip_Line;
+
+   Put("C1 Imag Part: ");
+   Get(B);
+   Skip_Line;
+
+   Put("C2 Real Part: ");
+   Get(C);
+   Skip_Line;
+
+   Put("C2 Imag Part: ");
+   Get(D);
+   Skip_Line;
+
+   C1 := (Real => A, Imag => B);
+   C2 := (Real => C, Imag => D);
 
    Complex_Numbers.Add      (C1, C2);
    Complex_Numbers.Subtract (C1, C2);
